afb3f9fe8768d7b4ab58ca721d762b9535c2089d
WildElaeagnus <54314123+akorzunin@users.noreply.github.com>
2022-07-24 23:48:10 +0300

    handle not valid xml files

--- test/test_main.py
+++ test/test_main.py
@@ -9,7 +9,10 @@ PWD = os.path.abspath(os.getcwd())
 sys.path.insert(1, PWD)
 
 from main import app
-from src.validation import col_descriptions
+from src.validation import Validator
+
+validator = Validator()
+col_descriptions = validator.csv_fields
 
 
 def collect_paths_to_test_files(dir_path: str, extension: str) -> dict[str, list]:
@@ -34,11 +37,6 @@ test_files = collect_paths_to_test_files(
     dir_path='./test/test_files', 
     extension='.xml',
 )
-bad_test_files = collect_paths_to_test_files(
-    dir_path='./test/test_bad_files', 
-    extension='.test',
-)
-
 
 file_paths = pytest.mark.parametrize(
     "file_path",
@@ -47,34 +45,17 @@ file_paths = pytest.mark.parametrize(
         *test_files['abs_file_paths'],
     ]
 )
-bad_file_paths = pytest.mark.parametrize(
-    "bad_file_path",
-    [
-        *bad_test_files['rel_file_paths'],
-        # *bad_test_files['abs_file_paths'],
-    ]
-)
-
-def move_bad_file_back(file_path: str):
-    os.replace(
-        os.path.join(
-            os.path.dirname(file_path),
-            'bad',
-            os.path.basename(file_path)
-        ),
-        file_path, 
-    )
 
 @pytest.fixture
 def parser():
     '''Create parser object'''
     return app
 
-@pytest.fixture
-def output() -> list:
+@pytest.fixture(params=test_files['abs_file_paths'])
+def output(request) -> list:
     '''Get path to pased file'''
     return app.parse_file(
-        file_path=test_files['abs_file_paths'][0]
+        file_path=request.param
     ), app
 
 @file_paths
@@ -116,7 +97,6 @@ def test_cli_run(file_path: str) -> None:
     process = subprocess.run(["python", "main.py", file_path])
     assert not process.returncode
 
-# TODO cant use output fixture here
 def test_csv_output_loc(output: list) -> None:
     '''csv file must be in same directory as input file and have same name'''
     output_file_path, app = output
@@ -136,18 +116,6 @@ def test_xml_input_loc(output: list) -> None:
     assert os.path.isfile(os.path.join(folder, 'arh', base)) or\
         os.path.isfile(os.path.join(folder, 'bad', base))
 
-def test_csv_fields(output: list) -> None:
-    output_file_path, app = output
-    df = pd.read_csv(
-        filepath_or_buffer=output_file_path,
-        delimiter=';',
-        header=None,
-        names=col_descriptions.values(),
-        encoding=app.encoding,
-    )
-    # TODO refactor this test
-    assert len(df.columns) == len(col_descriptions)
-
 def test_duplicates(output: list) -> None:
     '''csv output should not contain duplicates of fiels personal_account and period'''
     output_file_path, app = output
@@ -166,16 +134,9 @@ def test_duplicates(output: list) -> None:
         ],
     ).any()
 
-# TODO validation
-def test_csv_field_validation() -> None:
-    # mb use pydantic to validate all csv rows as models
-    ...
+def test_csv_field_validation(output: list) -> None:
+    # pydantic validate all csv rows as models
+    output_file_path, app = output
 
-def test_not_key_field_missing() -> None:
-    # handle missing key as blank field
-    ...
 
-def test_key_field_missing() -> None:
-    # skip row
-    ...
 
